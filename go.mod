c6e2fbb75b66d8463a783ffa7753a4972a58946b
Tomasz Wiszkowski <tomasz.wiszkowski@gmail.com>
2022-04-17 17:13:31 -0700

    Source hooks from simple JSON file. Allow selection.
    Change-Id: If115c0e52e91fb15d258f8d88be31e28134c6da7

--- go.mod
+++ go.mod
@@ -4,6 +4,14 @@ go 1.18
 
 require (
 	github.com/chzyer/readline v1.5.0 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/tview v0.0.0-20220307222120-9994674d60a8 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,35 +1,78 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 
-	"github.com/manifoldco/promptui"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
+type Config struct {
+	PostCommit []*Hook `json:"postCommit"`
+}
+
+const (
+	CategoryRoot = iota
+	CategoryPostCommit
+)
+
+type Reference struct {
+	category int
+	hook *Hook
+}
+
+var config Config
+
+func add(target *tview.TreeNode, ref *Reference) {
+	if ref.category == CategoryRoot {
+		node := tview.NewTreeNode("Post-commit").SetReference(&Reference{CategoryPostCommit, nil}).SetSelectable(true).SetColor(tcell.ColorGrey)
+		target.AddChild(node)
+		add(node, node.GetReference().(*Reference))
+	}
+
+	if ref.category == CategoryPostCommit && ref.hook == nil {
+		for _, h := range config.PostCommit {
+			node := tview.NewTreeNode("").SetReference(&Reference{CategoryPostCommit, h}).SetSelectable(true)
+			h.updateNode(node)
+			target.AddChild(node)
+		}
+	}
+}
+
+func onTreeNodeSelected(node *tview.TreeNode) {
+	reference := node.GetReference().(*Reference)
+
+	// Check if node or leaf. Nodes have no hook references.
+	if hook := reference.hook; hook == nil {
+		// This is a node.
+		// Collapse if visible, expand if collapsed.
+		node.SetExpanded(!node.IsExpanded())
+	} else {
+		// This is a leaf.
+		hook.toggleSelected()
+		hook.updateNode(node)
+	}
+}
+
 func main() {
-	items := []string{"Vim", "Emacs", "Sublime", "VSCode", "Atom"}
-	index := -1
-	var result string
 	var err error
+	var configFile []byte
 
-	for index < 0 {
-		prompt := promptui.SelectWithAdd{
-			Label:    "What's your text editor",
-			Items:    items,
-			AddLabel: "Other",
-		}
-
-		index, result, err = prompt.Run()
-
-		if index == -1 {
-			items = append(items, result)
-		}
+	if configFile, err = ioutil.ReadFile("hooks.json"); err != nil {
+		panic(err)
 	}
 
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
+	if err = json.Unmarshal(configFile, &config); err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("You choose %s\n", result)
+	root := tview.NewTreeNode("Hooks").SetColor(tcell.ColorGrey)
+	tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
+	tree.SetSelectedFunc(onTreeNodeSelected)
+	add(root, &Reference{CategoryRoot, nil})
+
+	if err := tview.NewApplication().SetRoot(tree, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
 }
